85987dc6d540afecdf416660d39fd1fcb6e75f0a
Anthony Adedayo <tonykay001@gmail.com>
2023-03-12 09:20:59 +0100

    Home page update

---
+++ assets/js/youtube.api.js
@@ -0,0 +1,118 @@
+const userAction = async () => {
+  const response = await fetch(
+    "https://www.googleapis.com/youtube/v3/search?key=AIzaSyA9lxKVAVsVoUjvYg12hJEYRvqD_7gdtlw&channelId=UCEO_A1gEiGZfsZaw6ssucAQ&part=snippet,id&order=date&maxResults=20",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+  console.log("youtube videos", myJson);
+  let videoHtml = "";
+  let onevideoHtml = "";
+  myJson.items.forEach((item) => {
+    videoHtml += `<div>
+            <div class="event">
+              <img
+                src="${item.snippet.thumbnails.medium.url}"
+                alt="Event Image 2"
+              />
+
+              <div class="event-data">
+                <p class="text-white">Stom Park New York</p>
+
+                <h4>
+                  <a href="https://www.youtube.com/watch?v=${item.id.videoId}"
+                    >${item.snippet.title}</a
+                  >
+                </h4>
+
+                <ul>
+                  <li>
+                    <img
+                      src="assets/images/calendar.svg"
+                      alt="calendar 2"
+                    />
+                  </li>
+
+                  <li>
+                    <img src="assets/images/clock.svg" alt="clock 2" />1:00 pm -
+                    2:00 pm
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>`;
+  });
+
+  onevideoHtml = `<div class="sermon" data-aos="zoom-in-right" data-aos-duration="1000">
+            <div class="sermon-img">
+              <div class="sermon-media">
+                <img
+                  src="assets/images/church/sermon-bg.jpeg"
+                  alt="Sermon Image"
+                />
+
+                <video controls>
+                  <source src="assets/videos/video.mp4" type="video/mp4" />
+
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+
+              <ul>
+                <li>
+                  <a class="s_audio" href="JavaScript:void(0)"
+                    ><img src="assets/images/music-note.svg" alt="volume"
+                  /></a>
+                </li>
+
+                <li>
+                  <a class="s_video" href="JavaScript:void(0)"
+                    ><img
+                      src="assets/images/play-button-2.svg"
+                      alt="Play Button"
+                  /></a>
+                </li>
+
+                <li>
+                  <a class="s_pdf" href="JavaScript:void(0)"
+                    ><img src="assets/images/book.svg" alt="Book"
+                  /></a>
+                </li>
+
+                <li>
+                  <a class="s_music" href="JavaScript:void(0)"
+                    ><img src="assets/images/download.svg" alt="download"
+                  /></a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="sermon-data">
+              <ul>
+                <li>Jorge karri</li>
+
+                <li>Nov 19, 2021</li>
+              </ul>
+
+              <h3>
+                <a href="https://www.youtube.com/watch?v=${myJson.items[0].id.videoId}"
+                  >${myJson.items[0].snippet.title}</a
+                >
+              </h3>
+
+              <p>
+               ${myJson.items[0].snippet.description}
+              </p>
+            </div>
+          </div>`;
+
+  document.getElementById("video-slider").innerHTML = videoHtml;
+  document.getElementById("one-video-sec").innerHTML = onevideoHtml;
+};
+
+userAction();
