070b2e5202ad8a7574b9927fafb93f1404ef857d
Anthony Adedayo <tonykay001@gmail.com>
2023-07-25 22:27:02 +0100

    code clean up

--- assets/js/youtube.api.js
+++ assets/js/youtube.api.js
@@ -1,126 +1,90 @@
-const userAction = async () => {
-  const response = await fetch(
-    "https://www.googleapis.com/youtube/v3/search?key=AIzaSyA9lxKVAVsVoUjvYg12hJEYRvqD_7gdtlw&channelId=UCEO_A1gEiGZfsZaw6ssucAQ&part=snippet,id&order=date&maxResults=20",
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
+const API_KEY = "AIzaSyA9lxKVAVsVoUjvYg12hJEYRvqD_7gdtlw";
+const CHANNEL_ID = "UCEO_A1gEiGZfsZaw6ssucAQ";
+
+const maxResults = 10;
+let slides = []; // Declare 'slides' at a broader scope
+let slideIndex = 0;
+// Function to fetch YouTube video details using the API
+async function fetchYouTubeVideos() {
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${maxResults}`
+    );
+    const data = await response.json();
+    return data.items;
+  } catch (error) {
+    console.error("Error fetching YouTube videos:", error);
+    return [];
+  }
+}
+
+// Function to create a carousel slide with video thumbnail, title, and link to YouTube
+function createCarouselSlide(video) {
+  const slide = document.createElement("div");
+  slide.classList.add("slide");
+
+  const videoId = video.id.videoId;
+  const thumbnailUrl = video.snippet.thumbnails.medium.url;
+  const title = video.snippet.title;
+  const uploadDate = new Date(video.snippet.publishedAt).toLocaleDateString(
+    "en-US",
+    { year: "numeric", month: "long", day: "numeric" }
   );
-  const myJson = await response.json(); //extract JSON from the http response
-  // do something with myJson
-  let videoHtml = "";
-  let onevideoHtml = "";
-  myJson.items.forEach((item) => {
-    const postDate = new Date(item.snippet.publishedAt);
-    const date =
-      postDate.getDate() +
-      "-" +
-      (postDate.getMonth() + 1) +
-      "-" +
-      postDate.getFullYear();
 
-    var hours = postDate.getHours();
-    var AmOrPm = hours >= 12 ? "pm" : "am";
-    hours = hours % 12 || 12;
-    var minutes = postDate.getMinutes();
-    var time = "Time  - " + hours + ":" + minutes + " " + AmOrPm;
+  slide.innerHTML = `
+    <a href="https://www.youtube.com/watch?v=${videoId}" target="_blank" rel="noopener noreferrer">
+      <img src="${thumbnailUrl}" alt="${title}">
+    </a>
+    <div class="slide-content">
+      <div class="slide-title">${title}</div>
+      <div class="slide-date">Uploaded on ${uploadDate}</div>
+    </div>
+  `;
 
-    videoHtml += `<div>
-            <div class="event">
-              <img
-                src="${item.snippet.thumbnails.medium.url}"
-                alt="Event Image 2"
-              />
+  return slide;
+}
 
-              <div class="event-data">
-              
-                <h4>
-                  <a href="https://www.youtube.com/watch?v=${item.id.videoId}"
-                    >${item.snippet.title}</a
-                  >
-                </h4>
+// Function to show the current slide and hide the rest
+function showSlide(index) {
+  slides.forEach((slide, idx) => {
+    if (idx === index) {
+      slide.style.display = "block";
+      slide.style.opacity = 1;
+    } else {
+      slide.style.display = "none";
+      slide.style.opacity = 0;
+    }
+  });
+}
 
-                <ul>
-                  <li>
-                    <img
-                      src="assets/images/calendar.svg"
-                      alt="calendar 2"
-                    />${date}
-                  </li>
+// Function to handle the previous slide
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+  showSlide(slideIndex);
+}
 
-                  <li>
-                    <img src="assets/images/clock.svg" alt="clock 2" />${time}
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>`;
+// Function to handle the next slide
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.length;
+  showSlide(slideIndex);
+}
+
+// Call the initialization function once the DOM is loaded
+document.addEventListener("DOMContentLoaded", async () => {
+  const carouselContainer = document.querySelector(".carousel");
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+
+  const videos = await fetchYouTubeVideos();
+  slides = videos.map(createCarouselSlide); // Store the slides in the 'slides' array
+  slides.forEach((slide, index) => {
+    slide.style.display = index === 0 ? "block" : "none";
+    carouselContainer.appendChild(slide);
   });
 
-  onevideoHtml = `<div class="sermon" data-aos="zoom-in-right" data-aos-duration="1000">
-            <div class="sermon-img">
-              <div class="sermon-media">
-                <img
-                  src="assets/images/church/sermon-bg.jpeg"
-                  alt="Sermon Image"
-                />
+  let slideIndex = 0;
+  showSlide(slideIndex);
 
-                <video controls>
-                  <source src="assets/videos/video.mp4" type="video/mp4" />
-
-                  Your browser does not support the video tag.
-                </video>
-              </div>
-
-              <ul>
-                <li>
-                  <a class="s_audio" href="JavaScript:void(0)"
-                    ><img src="assets/images/music-note.svg" alt="volume"
-                  /></a>
-                </li>
-
-                <li>
-                  <a class="s_video" href="JavaScript:void(0)"
-                    ><img
-                      src="assets/images/play-button-2.svg"
-                      alt="Play Button"
-                  /></a>
-                </li>
-
-                <li>
-                  <a class="s_pdf" href="JavaScript:void(0)"
-                    ><img src="assets/images/book.svg" alt="Book"
-                  /></a>
-                </li>
-
-                <li>
-                  <a class="s_music" href="JavaScript:void(0)"
-                    ><img src="assets/images/download.svg" alt="download"
-                  /></a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="sermon-data">
-              <h3>
-                <a href="https://www.youtube.com/watch?v=${myJson.items[0].id.videoId}"
-                  >${myJson.items[0].snippet.title}</a
-                >
-              </h3>
-
-              <p>
-               ${myJson.items[0].snippet.description}
-              </p>
-            </div>
-          </div>`;
-
-  document.getElementById("video-slider").innerHTML =
-    "<div id='video-slider' class='events-carousal-slider'>" +
-    videoHtml +
-    "</div>";
-  document.getElementById("one-video-sec").innerHTML = onevideoHtml;
-};
-
-userAction();
+  prevButton.addEventListener("click", prevSlide);
+  nextButton.addEventListener("click", nextSlide);
+});
